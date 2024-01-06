46632d21e5a63da3728f041aa3f8e20367ec1310
Ejiscoexpress <ejiscoexpress@gmail.com>
2024-01-06 15:54:35 +0100

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,37 @@
+ export async function getAllCharacters() {
+    const data = await fetch(`${endpoint}/characters`)
+  
+    if (!data.ok) {
+      throw new Error('Failed to fetch data')
+    }
+  
+    return data.json()
+  }
+  
+  export default async function Page() {
+    const data = await getAllCharacters()
+  
+    return (
+      <main>
+        <Container className="grid grid-cols-2 gap-1 py-5 md:grid-cols-3 lg:grid-cols-4">
+          {data?.characters?.map(item => {
+            return (
+              <Link
+                href={`/characters/${item.slug}`}
+                key={item.name}
+                className="overflow-hidden rounded-md"
+              >
+                <Image
+                  src={item.avatar}
+                  alt=""
+                  className="transition-all duration-500 hover:scale-110 hover:-rotate-2"
+                  width={500}
+                  height={500}
+                />
+              </Link>
+            )
+          })}
+        </Container>
+      </main>
+    )
+  }
